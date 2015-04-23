232aaecc3d1e72ab357a91d9468d0ef41018937e
HugoFAC <ei10073@fe.up.pt>
2015-04-23 18:16:27 +0100

    escolhe direçao em que se quer mover a peça
    verifica se a peça sai do tabuleiro, ou se cai na casa vencedora do
    oponente

--- src/gameLogic/Game.java
+++ src/gameLogic/Game.java
@@ -15,22 +15,25 @@ public class Game {
 	
 	public void start(){
 		Scanner sc = new Scanner(System.in); 
-		System.out.println("Que tipo de jogo deseja?\n1.Computador vs Computador\n2.Jogador vs Computador\n3.JogadorvsJogador");
+		System.out.println("Que tipo de jogo deseja?\n"
+				+ "1.Computador vs Computador\n"
+				+ "2.Jogador vs Computador\n"
+				+ "3.JogadorvsJogador");
 		int mode=sc.nextInt();
 		
 		if(mode == 3){
-			
-			System.out.println("Nome do Jogador1:\n");
-			while(sc.nextLine() == null){} //porque estava a ler em branco e o jogador1 ficava sem nome
+			System.out.println("Nome do Jogador1:");
+			while(sc.nextLine() == null){} //porque esta a ler em branco e o jogador1 fica sem nome
 			String name1=sc.nextLine();
 			p1 = new Player(name1,"B");
 			
-			System.out.println("Nome do Jogador2:\n");
+			System.out.println("Nome do Jogador2:");
 			String name2=sc.nextLine();
 			p2 = new Player(name2,"R");
 		}
 		gameBoard.init();
 		play(p1);
+		sc.close();
 	}
 	
 	public String getP1Name(){
@@ -47,10 +50,29 @@ public class Game {
 		System.out.println(player.getName()+" wich piece you wish to move?");
 		int position = sc.nextInt();
 		while(!validPick(position,p1)){
-			System.out.println(player.getName()+" wich piece you wish to move?");
+			System.out.println(player.getName()+" which piece you wish to move?");
 			position = sc.nextInt();
 		}
-		System.out.println(position);
+		System.out.println("In which direction you wish to move the selected piece?\n"
+				+ "1-northeast\n"
+				+ "2-east\n"
+				+ "3-southeast\n"
+				+ "4-southwest\n"
+				+ "5-west\n"
+				+ "6-northwest\n");
+		int direction = sc.nextInt();
+		while(!validDirection(position, direction)){
+			System.out.println("In which direction you wish to move the selected piece?\n"
+					+ "1-northeast\n"
+					+ "2-east\n"
+					+ "3-southeast\n"
+					+ "4-southwest\n"
+					+ "5-west\n"
+					+ "6-northwest\n");
+			direction = sc.nextInt();
+		}
+		System.out.println(direction);
+		sc.close();
 	}
 	
 	public boolean validPick(int position,Player player){
@@ -91,15 +113,108 @@ public class Game {
 		if(gameBoard.getBoard().getNode(position+10)!=null && gameBoard.getBoard().getNode(position+10).getName() == opponent) //check southeast
 			return true;
 		
-		if(gameBoard.getBoard().getNode(position-10)!=null && gameBoard.getBoard().getNode(position-10).getName() == opponent) //check southwest
+		if(gameBoard.getBoard().getNode(position-1)!=null && gameBoard.getBoard().getNode(position-1).getName() == opponent) //check southwest
 			return true;
 		
 		if(gameBoard.getBoard().getNode(position-11)!=null && gameBoard.getBoard().getNode(position-11).getName() == opponent) //check west
 			return true;
 		
-		if(gameBoard.getBoard().getNode(position-1)!=null && gameBoard.getBoard().getNode(position-1).getName() == opponent) //check norththwest
+		if(gameBoard.getBoard().getNode(position-10)!=null && gameBoard.getBoard().getNode(position-10).getName() == opponent) //check norththwest
 			return true;
 		
 		return false;
+	}
+	
+	public boolean validDirection(int position, int direction){
+		
+		int newPosition = directionEnd(position, direction);
+		
+		String ally = gameBoard.getBoard().getNode(position).getName();
+		String ending;
+		
+		if(ally == "B")
+			ending = "L";
+		else
+			ending = "W";
+		
+		
+		if(gameBoard.getBoard().getNode(newPosition)==null){
+			if(direction == 1)
+				System.out.println("You cant move the selected piece in the northeast direction, as it would leave the board!\n");
+			else if(direction == 2)
+				System.out.println("You cant move the selected piece in the east direction, as it would leave the board!\n");
+			else if(direction == 3)
+				System.out.println("You cant move the selected piece in the southeast direction, as it would leave the board!\n");
+			else if(direction == 4)
+				System.out.println("You cant move the selected piece in the southwest direction, as it would leave the board!\n");
+			else if(direction == 5)
+				System.out.println("You cant move the selected piece in the west direction, as it would leave the board!\n");
+			else if(direction == 6)
+				System.out.println("You cant move the selected piece in the northwest direction, as it would leave the board!\n");
+
+			return false;
+		}
+		else if(gameBoard.getBoard().getNode(newPosition).getName()==ending){
+			if(direction == 1)
+				System.out.println("You cant move the selected piece in the northeast direction, as it would land on your opponents winning spot!\n");
+			else if(direction == 2)
+				System.out.println("You cant move the selected piece in the east direction, as it would land on your opponents winning spot!\n");
+			else if(direction == 3)
+				System.out.println("You cant move the selected piece in the southeast direction, as it would land on your opponents winning spot!\n");
+			else if(direction == 4)
+				System.out.println("You cant move the selected piece in the southwest direction, as it would land on your opponents winning spot!\n");
+			else if(direction == 5)
+				System.out.println("You cant move the selected piece in the west direction, as it would land on your opponents winning spot!\n");
+			else if(direction == 6)
+				System.out.println("You cant move the selected piece in the northwest direction, as it would land on your opponents winning spot!\n");
+			return false;
+		}
+		return true;
+	}
+	
+	public int directionEnd(int position, int direction){
+		
+		int newPosition = position;
+		
+		String ally = gameBoard.getBoard().getNode(position).getName();
+		
+		if(direction == 1){ //northeast +1
+			newPosition++;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally) 
+				newPosition++;
+		}
+		else if(direction == 2){ //east +11
+			newPosition+=11;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally)
+				newPosition+=11;
+		}
+		else if(direction == 3){ //southeast +10
+			newPosition+=10;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally) 
+				newPosition+=10;
+		}
+		else if(direction == 4){ //southwest -1
+			newPosition--;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally)
+				newPosition--;
+		}
+		else if(direction == 5){ //west -11
+			newPosition-=11;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally)
+				newPosition-=11;
+		}
+		else if(direction == 6){ //northwest -10
+			newPosition-=10;
+			
+			while(gameBoard.getBoard().getNode(newPosition)!=null && gameBoard.getBoard().getNode(newPosition).getName() == ally)
+				newPosition-=10;
+		}
+		
+		return newPosition;
 	}
 }
